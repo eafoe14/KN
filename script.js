5b6cd0f88a153e9311ab39f4560041684963bb38
unknown <eafoe@vk.com>
2024-01-31 01:05:48 +0300

    Добавлен вариант игры 10x10 для двух игроков (2player10x10)

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function btn_click1() {
 }
 
 function btn_click2() {
-    window.location.href = '1player10x10/index.html';
+    window.location.href = '2player10x10/index.html';
 }
 
 function btn_click3() {
-    window.location.href = '2player10x10/index.html';
+    window.location.href = '1player10x10/index.html';
 }
